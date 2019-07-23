095aacfa16bf3c96c88da0ba1c1d612ef9be36fd
tubone24 <tubo.yyyuuu@gmail.com>
2019-07-23 22:15:26 +0900

    build typecsript

--- ebook_homebrew/rest.py
+++ ebook_homebrew/rest.py
@@ -85,7 +85,11 @@ class FileNotFoundRespSchema(Schema):
     errorDate = fields.Date()
 
 
+# Vue Sampled
 api.add_route("/", static=True)
+api.add_route("/js", static=True)
+api.add_route("/css", static=True)
+api.add_route("/img", static=True)
 
 
 @api.route("/status")
